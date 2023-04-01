93178b9beaab233d217ae6bb4f60067536aa2d2b
raoyalong <ryl_che@163.com>
2023-04-02 00:15:55 +0800

    测试github连接

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author raoyalong
+ * @create 2023 - 04 - 02 0:14
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试github连接");
+    }
+}
